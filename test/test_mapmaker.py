954a885871e2fdb109d4b1cda36909446bfd6e24
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2023-09-19 15:49:20 -0500

    Propagated changes into mapmaker.

--- test/test_mapmaker.py
+++ test/test_mapmaker.py
@@ -14,33 +14,55 @@ class TestGlobal(unittest.TestCase):
     def setUp(self):
 
         flight_name = '220513-FH135'
-        self.metadata_fp = os.path.join('./test/test_data', flight_name,
-                                        'CollatedImageLog.csv')
+        self.metadata_dir = os.path.join('./test/test_data', flight_name)
         self.image_dir = os.path.join('./test/test_data', flight_name,
                                       'images/23085686')
+        self.img_log_fp = os.path.join(self.metadata_dir, 'image.log')
+        self.imu_log_fp = os.path.join(self.metadata_dir, 'OBC/PresIMULog.csv')
+        self.gps_log_fp = os.path.join(self.metadata_dir, 'OBC/GPSLog.csv')
 
     def test_mapmake(self):
 
-        mm = mapmaker.Mapmaker()
+        mm = mapmaker.Mapmaker(
+            image_dir=self.image_dir,
+            img_log_fp=self.img_log_fp,
+            imu_log_fp=self.imu_log_fp,
+            gps_log_fp=self.gps_log_fp,
+        )
 
         mm.mapmake()
 
-    def test_preprocess(self):
+    def test_prep(self):
 
-        mm = mapmaker.Mapmaker()
+        mm = mapmaker.Mapmaker(
+            image_dir=self.image_dir,
+            img_log_fp=self.img_log_fp,
+            imu_log_fp=self.imu_log_fp,
+            gps_log_fp=self.gps_log_fp,
+        )
 
-        mm.preprocess()
+        mm.prep()
 
     def test_georeference(self):
 
-        mm = mapmaker.Mapmaker()
+        mm = mapmaker.Mapmaker(
+            image_dir=self.image_dir,
+            img_log_fp=self.img_log_fp,
+            imu_log_fp=self.imu_log_fp,
+            gps_log_fp=self.gps_log_fp,
+        )
 
         mm.preprocess()
         mm.georeference()
 
     def test_construct_mosaic(self):
 
-        mm = mapmaker.Mapmaker()
+        mm = mapmaker.Mapmaker(
+            image_dir=self.image_dir,
+            img_log_fp=self.img_log_fp,
+            imu_log_fp=self.imu_log_fp,
+            gps_log_fp=self.gps_log_fp,
+        )
 
         mm.preprocess()
         mm.georeference()
