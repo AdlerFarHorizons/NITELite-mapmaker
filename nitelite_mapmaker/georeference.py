fdf171ffae4f26a6fff78a52e27e929209cf096d
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2023-09-26 17:17:13 -0500

    Got a basic resampler working.

--- nitelite_mapmaker/georeference.py
+++ nitelite_mapmaker/georeference.py
@@ -1,13 +1,63 @@
 '''This module handles georeferencing.
 '''
 
+from typing import Tuple
+
 import numpy as np
 
 
 class GeoReferencer:
 
-    def resample(self, img: np.ndarray) -> np.ndarray:
+    def resample(
+        self,
+        points: list[np.ndarray, np.ndarray],
+        img: np.ndarray,
+        res: float = None,
+    ) -> Tuple[list[np.ndarray, np.ndarray], np.ndarray]:
+        '''
+        Args:
+            points: x and y values for the pixels. Currently assumed that
+                the grid is rectilinear. Adapting the code to work with
+                non-rectilinear points may require doing an average of the
+                cross products between neighboring pixels (to get the area)
+                or using a triangulated irregular network.
+            img: The array values themselves
+            res: 1D resolution of the output. Currently defaults to the
+                geometric mean of the input image x and y resoluion.
 
-        transformed_img = img.copy()
+        Returns:
+            points_resampled: x and y values for the resampled pixels.
+            img_resampled: Resampled image.
+        '''
 
-        return transformed_img
+        # Calculate the weights (flux-conserved)
+        xs, ys = points
+        da = np.linalg.norm(np.cross(
+            [xs[0, 1] - xs[0, 0], ys[0, 1] - ys[0, 0]],
+            [xs[1, 0] - xs[0, 0], ys[1, 0] - ys[0, 0]],
+        ))
+        weights = img.flatten() * da
+
+        # Setup output image
+        if res is None:
+            res = np.sqrt(da)
+        x_bins = np.arange(xs.min() - res / 2., xs.max() + res / 2., res)
+        y_bins = np.arange(ys.min() - res / 2., ys.max() + res / 2., res)
+
+        # Resample (currently just binning w/ a histogram)
+        img_resampled, _, _ = np.histogram2d(
+            xs.flatten(),
+            ys.flatten(),
+            bins=[x_bins, y_bins],
+            weights=weights
+        )
+
+        # Normalize bins
+        da_resampled = (x_bins[1] - x_bins[0]) * (y_bins[1] - y_bins[0])
+        img_resampled /= da_resampled
+
+        # Get the coordinates out
+        xs_resampled = 0.5 * (x_bins[:-1] + x_bins[1:])
+        ys_resampled = 0.5 * (y_bins[:-1] + y_bins[1:])
+
+        return (xs_resampled, ys_resampled), img_resampled
--- test/test_georeferencer.py
+++ test/test_georeferencer.py
@@ -19,11 +19,33 @@ class TestGlobal(unittest.TestCase):
     def test_conserved(self):
 
         original = self.rng.uniform(size=(4, 6))
-        transformed = self.greffer.resample(original)
+        xs_original_frame = np.linspace(0., 10., original.shape[1])
+        ys_original_frame = np.linspace(0., 10., original.shape[0])
+        da_original_frame = (
+            (xs_original_frame[1] - xs_original_frame[0])
+            * (ys_original_frame[1] - ys_original_frame[0])
+        )
+        integrated = da_original_frame.sum() * da_original_frame
 
-        # Should sum to the same
-        np.testing.assert_allclose(original.sum(), transformed.sum())
+        xs_original_frame_mesh, ys_original_frame_mesh = np.meshgrid(
+            xs_original_frame,
+            ys_original_frame,
+        )
 
-        # But they shouldn't be the same
-        is_matching = np.isclose(original, transformed)
-        assert is_matching.sum() != is_matching.size
+        (xs_resampled, ys_resampled), resampled = self.greffer.resample(
+            [xs_original_frame_mesh, ys_original_frame_mesh],
+            original,
+        )
+
+        # Check that the integrals match
+        da_resampled = (
+            (xs_resampled[1] - xs_resampled[0])
+            * (ys_resampled[1] - ys_resampled[0])
+        )
+        integrated_resampled = resampled.sum() * da_resampled
+        np.testing.assert_allclose(integrated, integrated_resampled, rtol=0.02)
+
+        # But the arrays shouldn't be the same
+        if original.shape == resampled.shape:
+            is_matching = np.isclose(original, resampled)
+            assert is_matching.sum() != is_matching.size
